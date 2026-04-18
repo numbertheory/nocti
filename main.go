cf65ab8e524b81048c164f68573eff7033c97717
JP Etcheber <jetcheber@gmail.com>
2026-04-18 15:03:22 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+// Version is a placeholder that will be overwritten at build time.
+var Version = "development"
+
+func main() {
+	// Define the --version flag
+	versionFlag := flag.Bool("version", false, "print the app version")
+
+	// Parse flags
+	flag.CommandLine.Usage = func()
+	flag.Parse()
+
+	// Logic for the version flag
+	if *versionFlag {
+		fmt.Println(Version)
+		return
+	}
+
+	// The app does nothing else and exits quietly if run alone.
+}
